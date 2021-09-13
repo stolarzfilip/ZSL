7ba37e9d743dfc878f1051609c22763d0076e2e2
stolarzfilip <90000918+stolarzfilip@users.noreply.github.com>
2021-09-13 10:04:50 +0200

    Add files via upload

--- operacjenazmiennych.php
+++ operacjenazmiennych.php
@@ -23,7 +23,30 @@
     }
     else {
         echo "różne";
-    }
+    } 
+
+   // postinkrementacja $x++ 
+   // preinkrementacja ++$x  
+   // postdekrementacja $x--
+   // predekrementacja --$x  
+
+ $x=2; 
+ echo "<hr>$x"; 
+ echo $x++; 
+ echo ++$x,"<br>";
+
+$x=1; 
+echo $x; // 1 
+$x=$x++; 
+echo $x; // 1
+$x=++$x; 
+echo $x; // 2
+$y=++$x; 
+echo $x; // 3
+echo $y; // 3
+$y=$x++; 
+echo $x; // 4
+echo "$y<br>"; // 3
 
 
 ?>
